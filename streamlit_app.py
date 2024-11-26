f7c3eba511d239a04c7d5fa44060521adb0a67fe
federents <nathanlemuel14@gmail.com>
2024-11-26 16:12:55 +0700

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,16 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("_MENGHITUNG_ VOLUME TABUNG :blue[cool] :rocket:")
+
+jari = st.number_input("Masukkan jari-jari (r):", min_value=0.0, step=0.1)
+
+# Input tinggi
+tinggi = st.number_input("Masukkan tinggi (t):", min_value=0.0, step=0.1)
+
+# Tombol hitung
+if st.button("Hitung Volume"):
+    if radius > 0 and height > 0:
+        volume = math.pi * jari**2 * tinggi
+        st.success(f"Volume tabung adalah: {volume:.2f}")
+    else:
+        st.error("Jari-jari dan tinggi harus lebih besar dari 0!")
