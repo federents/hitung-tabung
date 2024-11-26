f441d0dc53b83f932664c30baa647245d2dd5f03
federents <nathanlemuel14@gmail.com>
2024-11-26 16:20:01 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ tinggi = st.number_input("Masukkan tinggi (t):", min_value=0.0, step=0.1)
 
 # Tombol hitung
 if st.button("Hitung Volume"):
-    if radius > 0 and height > 0:
+    if jari > 0 and tinggi > 0:
         volume = math.pi * jari**2 * tinggi
         st.success(f"Volume tabung adalah: {volume:.2f}")
     else:
